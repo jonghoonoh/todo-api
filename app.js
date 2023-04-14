3594d30332fd4309df3ab5ead0e03994368ed719
jonghoonoh <jho@codeit.kr>
2023-04-14 16:33:36 +0900

    02_06

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import tasks from './data/mock.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/tasks', (req, res) => {
   /** 쿼리 파라미터
@@ -23,6 +24,27 @@ app.get('/tasks', (req, res) => {
   }
 
   res.send(newTasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send({ message: '해당 id를 찾을 수 없습니다.' });
+  }
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = req.body;
+  const ids = tasks.map((task) => task.id)
+  newTask.id = Math.max(...ids) + 1;
+  newTask.isComplete = false;
+  newTask.createdAt = new Date();
+  newTask.updatedAt = new Date();
+  tasks.push(newTask);
+  res.send(newTask);
 });
 
 app.listen(3000, () => console.log('Server Started'));
