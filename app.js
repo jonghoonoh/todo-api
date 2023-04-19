bf5c662592d94aa7d3fc36ead3cac04742db736e
jonghoonoh <jho@codeit.kr>
2023-04-19 19:48:19 +0900

    03_06

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import tasks from './data/mock.js';
+import mockTasks from './data/mock.js';
 import { DATABASE_URL } from './constants.js';
+import Task from './models/task.js';
 
 mongoose.connect(DATABASE_URL).then(() => console.log('Connected to DB'));
 
 const app = express();
 app.use(express.json());
 
-app.get('/tasks', (req, res) => {
+app.get('/tasks', async (req, res) => {
   /** 쿼리 파라미터
    *  - sort: 'oldest'인 경우 오래된 태스크 기준, 나머지 경우 새로운 태스크 기준
    *  - count: 태스크 개수
    */
   const sort = req.query.sort;
-  const count = Number(req.query.count);
+  const count = Number(req.query.count) || 0;
 
-  const sortFn =
-    sort === 'oldest'
-      ? (a, b) => a.createdAt - b.createdAt
-      : (a, b) => b.createdAt - a.createdAt;
+  const sortOption = { createdAt: sort === 'oldest' ? 'asc' : 'desc' };
+  const tasks = await Task.find().sort(sortOption).limit(count);
 
-  let newTasks = tasks.sort(sortFn);
-
-  if (count) {
-    newTasks = newTasks.slice(0, count);
-  }
-
-  res.send(newTasks);
+  res.send(tasks);
 });
 
-app.get('/tasks/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const task = tasks.find((task) => task.id === id);
+app.get('/tasks/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+
   if (task) {
     res.send(task);
   } else {
@@ -40,20 +33,14 @@ app.get('/tasks/:id', (req, res) => {
   }
 });
 
-app.post('/tasks', (req, res) => {
-  const newTask = req.body;
-  const ids = tasks.map((task) => task.id);
-  newTask.id = Math.max(...ids) + 1;
-  newTask.isComplete = false;
-  newTask.createdAt = new Date();
-  newTask.updatedAt = new Date();
-  tasks.push(newTask);
+app.post('/tasks', async (req, res) => {
+  const newTask = await Task.create(req.body);
   res.status(201).send(newTask);
 });
 
 app.patch('/tasks/:id', (req, res) => {
   const id = Number(req.params.id);
-  const task = tasks.find((task) => task.id === id);
+  const task = mockTasks.find((task) => task.id === id);
 
   if (task) {
     Object.keys(req.body).forEach((key) => {
@@ -67,9 +54,9 @@ app.patch('/tasks/:id', (req, res) => {
 
 app.delete('/tasks/:id', (req, res) => {
   const id = Number(req.params.id);
-  const idx = tasks.findIndex((task) => task.id === id);
+  const idx = mockTasks.findIndex((task) => task.id === id);
   if (idx >= 0) {
-    tasks.splice(idx, 1);
+    mockTasks.splice(idx, 1);
     res.sendStatus(204);
   } else {
     res.status(404).send({ message: '해당 id를 찾을 수 없습니다.' });
--- models/task.js
+++ models/task.js
@@ -4,6 +4,14 @@ const TaskSchema = new mongoose.Schema(
   {
     title: {
       type: String,
+      required: true,
+      maxLength: 30,
+      validate: {
+        validator: function (title) {
+          return title.split(' ').length > 1;
+        },
+        message: 'Must be at least 2 words',
+      },
     },
     description: {
       type: String,
